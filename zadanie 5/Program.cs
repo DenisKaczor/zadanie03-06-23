537ccffeb6e4332c61f170fc000dca672d9096cc
DenisKaczor <chx100210@student.chorzow.wsb.pl>
2023-06-03 16:58:38 +0200

    8zadań

---
+++ zadanie 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("wprowadź pierwszą liczbę");
+			double a = inputValue();
+			Console.WriteLine("wprowadź drugą liczbę");
+			double b = inputValue();
+			if(a>b)
+			{
+				Console.WriteLine("wynik: "+a);
+				Console.ReadKey();
+			}
+			else
+			{
+				Console.WriteLine("wynik: "+b);
+				Console.ReadKey();
+			}
+		}
+		private static double inputValue()
+		{
+			double result;
+			while (!double.TryParse(Console.ReadLine(), out result)) ;
+			return result;
+		}
+	}
+}
